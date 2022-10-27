e8bd23029d151d7394500973b86b56df0174a4e9
peter <wleowndud05@naver.com>
2022-10-27 20:44:44 +0900

    feat : reducer

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
+import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = (props) => {
+    const cartCtx = useContext(CartContext);
+
+    //  리듀스에서는 첫 번째 인자로 함수를 받는다,
+    //  리듀스에서는 두 번째 인자로 초기값(시작값)을 받는다,
+    //      리듀스에서 첫 번째 인자인 함수는
+    //          1번째 인자 : 초기값
+    //          2번째 인자 : 배열의 아이템 (반복문의 값)
+    const numberOfCartItems = cartCtx.items.reduce((carNumber, item) => {
+        return carNumber + item.amount;
+    }, 0);
+
+    console.log(numberOfCartItems);
+
     return (
         <button className={classes.button} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
             <span>Your Cart</span>
-            <span className={classes.badge}>3</span>
+            <span className={classes.badge}>{numberOfCartItems}</span>
         </button>
     );
 };
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,13 +1,82 @@
+import { useReducer } from 'react';
+
 import CartContext from './cart-context';
 
-const CartProvider = (props) => {
-    const addItemToCartHandler = (item) => {};
+//  리듀서의 초기값
+const cartInitialState = {
+    items: [],
+    totalAmount: 0,
+};
 
-    const removeItemFromCartHandler = (id) => {};
+/**
+ * 리듀서
+ * @param {*} state
+ * @param {*} action
+ * @returns
+ */
+const cartReducer = (state, action) => {
+    if (action.type === 'ADD') {
+        //  push는 기존 배열에 추가
+        //  concat은 새 배열을 만듬
+        //const updatedItems = [...state.items, action.item]
+        const updatedItems = state.items.concat(action.item);
+        const updatedTotalAmount =
+            state.totalAmount + action.item.totalAmount * action.item.amount;
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount,
+        };
+    }
+
+    return cartInitialState;
+
+    /*
+    switch (action) {
+        case 'ADD':
+            
+        case 'REMOVE':
+            
+            return ;
+        default:
+            return cartInitialState;
+    }
+    */
+};
+
+/**
+ *
+ * @param {} props
+ * @returns
+ */
+const CartProvider = (props) => {
+    //  state, action
+    const [cartState, dispatchCartAction] = useReducer(
+        cartReducer,
+        cartInitialState
+    );
+
+    //  dispatch의 첫번째 인자는 객체다
+    //  객채의 첫번째 속성을 type을 지정한다
+    //      action.type으로 조건을 처리한다
+    //             type의 값은 대문자로 한다
+    //      두번째 속성을 값을 지정
+    const addItemToCartHandler = (item) => {
+        dispatchCartAction({
+            type: 'ADD',
+            item: item,
+        });
+    };
+
+    const removeItemFromCartHandler = (id) => {
+        dispatchCartAction({
+            type: 'REMOVE',
+            id: id,
+        });
+    };
 
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemToCartHandler,
         removeItem: removeItemFromCartHandler,
     };
